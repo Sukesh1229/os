3abe844f4e9d571782ff84238b164f1fb30dd8ca
Sukesh Kumar <96759772+Sukesh1229@users.noreply.github.com>
2023-02-22 12:06:24 +0530

    Add files via upload

---
+++ LRU_pagerReplacement.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+ 
+int findLRU(int time[], int n)
+{
+   int i, minimum = time[0], pos = 0;
+ 
+   for (i = 1; i < n; ++i)
+   {
+      if (time[i] < minimum)
+      {
+         minimum = time[i];
+         pos = i;
+      }
+   }
+ 
+   return pos;
+}
+
+int main()
+{
+   int np,nf,counter = 0, time[10], flag1, flag2, i, j, pos, faults = 0;
+   printf("Enter number of frames: ");
+   scanf("%d", &nf);
+ 
+   printf("Enter number of pages: ");
+   scanf("%d", &np);
+   int frames[nf],pages[np];
+ 
+   printf("Enter the pages: ");
+ 
+   for (i = 0; i < np; ++i)
+   {
+      scanf("%d", &pages[i]);
+   }
+ 
+   for (i = 0; i < nf; ++i)
+   {
+      frames[i] = -1;
+   }
+ 
+ 	printf("\n\n");
+   for (i = 0; i < np; ++i)
+   {
+   	  printf("%d  Frames: ",pages[i]);
+      flag1 = flag2 = 0;
+ 
+      for (j = 0; j < nf; ++j)
+      {
+         if (frames[j] == pages[i])
+         {
+            counter++;
+            time[j] = counter;
+            flag1 = flag2 = 1;
+            break;
+         }
+      }
+ 
+      if (flag1 == 0)
+      {
+         for (j = 0; j < nf; ++j)
+         {
+            if (frames[j] == -1)
+            {
+               counter++;
+               faults++;
+               frames[j] = pages[i];
+               time[j] = counter;
+               flag2 = 1;
+               break;
+            }
+         }
+      }
+ 
+      if (flag2 == 0)
+      {
+         pos = findLRU(time, nf);
+         counter++;
+         faults++;
+         frames[pos] = pages[i];
+         time[pos] = counter;
+      }
+      for (j = 0; j < nf; ++j)
+      {
+         printf("%d  ", frames[j]);
+      }
+      printf("\n");
+   }
+ 	int hit=np-faults;
+   printf("\nTotal Page Faults = %d", faults);
+   printf("\nTotal page Hits = %d", hit);
+   printf("\nHit Ratio: %.2f\nFault Ratio: %.2f",(float)hit/np,(float)faults/np);
+ 
+   return 0;
+}
---
+++ Variable_partition_Worst_Fit.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	printf("enter the number of process: ");
+	scanf("%d",&n);
+	int m,size[n],a[n],store[n];
+	printf("enter the sizes of process: \n");
+	for(i=1;i<=n;i++)
+	{
+		printf("process-%d size: ",i);
+		scanf("%d",&size[i]);
+		store[i]=-1;
+	}
+	printf("enter the number of blocks in memory: ");
+	scanf("%d",&m);
+	int block[m],visit[m];
+	printf("Enter the sizes of each blocks: \n");
+	for(i=1;i<=m;i++)
+	{
+		printf("size of block-%d  ",i);
+		scanf("%d",&block[i]);
+		visit[i]=-1;
+	}
+	int k=1,count=0;
+	while(k<=n){
+		int min=0,flag=0;
+		for(i=1;i<=m;i++)
+		{
+			if(block[i]>min && block[i]>=size[k])
+			{
+				min=block[i];
+				flag=i;
+			}
+		}
+		if(flag!=0)
+		{
+			store[k]=flag;
+			visit[flag]=1;
+			block[flag]=block[flag]-size[k];
+			k++;
+		}
+		else
+		{
+			a[count]=k;
+			count++;
+			printf("process-%d is can not insert into memory\n",k);
+			k++;
+		}
+	}
+	printf("the process allocated are: \n");
+	for(i=1;i<k;i++)
+	{
+		if(store[i]!=-1)
+		{
+			printf("p%d: at block-%d\n",i,store[i]);
+		}
+	}
+	int sum=0;
+	printf("internal fragment of memory: ");
+	for(i=1;i<=m;i++)
+	{
+		if(visit[i]!=-1)
+		{
+			sum=sum+block[i];
+		}
+	}
+	printf("%d\n",sum);
+	if(count!=0)
+	{
+		sum=0;
+		for(i=1;i<=m;i++)
+		{
+			if(visit[i]==-1){
+				sum=sum+block[i];
+			}
+		}
+		for(i=0;i<count;i++)
+		{
+			if(sum>=size[a[i]])
+			{
+				break;
+			}
+		}
+		if(i<count)
+		{
+			printf("external segment: %d",sum);
+		}
+		else
+		{
+			printf("There is no external fragmentation");
+		}
+	}
+	else
+	{
+		printf("There is no external fragmentation");
+	}
+	
+}
